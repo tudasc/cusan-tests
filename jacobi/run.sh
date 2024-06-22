32ce4aa39b68ef8fff2ff8a955062a8df5a7d2ad
Alexander Hück <hueck.alexander@gmail.com>
2024-06-22 19:40:59 +0200

    Jacobi setup

--- jacobi/run.sh
+++ jacobi/run.sh
@@ -5,4 +5,10 @@ export CUCORR_KERNEL_DATA_FILE="/home/a_h_ck/workspace/cucorr-tests/jacobi/kerne
 
 export TSAN_OPTIONS="exitcode=0 suppressions=/home/a_h_ck/workspace/cucorr-tests/jacobi/scripts/suppression-lb2.txt"
 
-LD_PRELOAD="/home/a_h_ck/workspace/cucorr/install/cucorr/lib64/libCucorrMPIInterceptor.so" mpiexec -np 2 ./bin/jacobi_cuda_normal_mpi -t 2 1
+
+preflag=$1
+if [ -z "$preflag" ]; then
+    LD_PRELOAD="/home/a_h_ck/workspace/cucorr/install/cucorr/lib64/libCucorrMPIInterceptor.so" mpiexec -np 2 ./bin/jacobi_cuda_normal_mpi -t 2 1
+else
+    mpiexec -np 2 ./bin/jacobi_cuda_normal_mpi -t 2 1
+fi
