40cc703e7d202ec877ef9738fa1c063a50b7bf54
Alexander Hück <hueck.alexander@gmail.com>
2024-06-23 14:44:27 +0200

    Setup for tealeaf

--- tealeaf/run.sh
+++ tealeaf/run.sh
@@ -1,18 +1,21 @@
-set -xe
+#!/bin/bash
 
-export CUCORR_BUILD_DIR="/home/tz91sono/cucorr_main/cucorr/install/cucorr/"
-export TYPEART_TYPE_FILE=$PWD"/BUILD-cucorr/types.yaml"
+export TYPEART_TYPE_FILE="/home/a_h_ck/workspace/cucorr-tests/tealeaf/types-tealeaf.yaml"
+export CUCORR_KERNEL_DATA_FILE="/home/a_h_ck/workspace/cucorr-tests/tealeaf/kernel-tealeaf.yaml"
 
-export CUCORR_MPICXX_EXEC=$CUCORR_BUILD_DIR"bin/cucorr-mpic++-d"
-export CUCORR_MPICC_EXEC=$CUCORR_BUILD_DIR"bin/cucorr-mpicc-d"
-export CUCORR_MPI_PRELOAD=$CUCORR_BUILD_DIR"lib64/libCucorrMPIInterceptor-d.so"
+export TSAN_OPTIONS="exitcode=0 suppressions=/home/a_h_ck/workspace/cucorr-tests/tealeaf/scripts/suppression-lb2.txt"
 
 
+build_dir="build_cucorr"
+preflag=$1
+if [ ! -z "$preflag" ]; then
+  export $preflag
+  echo "Exported : $preflag"
+  build_dir="build_vanilla"
+fi
 
-TYPEART_WRAPPER=OFF cmake . \
-                    -DENABLE_MPI=ON -BBUILD-cucorr -DMODEL=cuda -DCUDA_ARCH=sm_70 -DCMAKE_BUILD_TYPE=Debug\
-                    -DMODEL=cuda -DCMAKE_CUDA_COMPILER=$CUCORR_MPICXX_EXEC -DCMAKE_CXX_COMPILER=$CUCORR_MPICXX_EXEC  -DCMAKE_C_COMPILER=$CUCORR_MPICC_EXEC
-
-cmake --build BUILD-cucorr -- -j1
-
-mpirun -np 2 ./BUILD-cucorr/cuda-tealeaf 
+if [ -z "$preflag" ]; then
+    LD_PRELOAD="/home/a_h_ck/workspace/cucorr/install/cucorr/lib64/libCucorrMPIInterceptor.so" mpiexec -np 2 ./${build_dir}/cuda-tealeaf 
+else
+    mpiexec -np 2 ./${build_dir}/cuda-tealeaf 
+fi
